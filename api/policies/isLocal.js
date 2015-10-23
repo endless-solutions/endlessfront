5cd29261cb2f3da8807be6402e0004c3ed97b31a
Satyender Yadav <satyenderyadav4993@gmail.com>
2015-10-24 01:26:27 +0530

    isLocal removed test prints

--- api/policies/isLocal.js
+++ api/policies/isLocal.js
@@ -1,8 +1,5 @@
 module.exports = function(req,res,cb) {
   var ALLOWED_HOST = process.env.PROXY_HOST || '127.0.0.1';
-  console.log("PPolicy invoked");
-  console.log(req.ip);
-  console.log(ALLOWED_HOST);
   if(req.ip == ALLOWED_HOST){
     cb();
   }
