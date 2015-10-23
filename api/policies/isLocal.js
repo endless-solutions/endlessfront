30682e0bf79c7a4e06639ad8efddbde48d98b478
Ubuntu <ubuntu@ip-172-31-34-186.us-west-2.compute.internal>
2015-10-23 19:51:20 +0000

    Environment Encapsulation

--- api/policies/isLocal.js
+++ api/policies/isLocal.js
@@ -1,5 +1,8 @@
 module.exports = function(req,res,cb) {
   var ALLOWED_HOST = process.env.PROXY_HOST || '127.0.0.1';
+  console.log("PPolicy invoked");
+  console.log(req.ip);
+  console.log(ALLOWED_HOST);
   if(req.ip == ALLOWED_HOST){
     cb();
   }
