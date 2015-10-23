46e7d70dbd76d8f94d6a106704814ba602e738e3
Satyender Yadav <satyenderyadav4993@gmail.com>
2015-10-24 01:58:00 +0530

    isLocal out

--- api/policies/isLocal.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(req,res,cb) {
-  var ALLOWED_HOST = process.env.PROXY_HOST || '127.0.0.1';
-  if(req.ip == ALLOWED_HOST){
-    cb();
-  }
-  res.forbidden(new Error ('Allows '+ALLOWED_HOST+' only.'));
-}
